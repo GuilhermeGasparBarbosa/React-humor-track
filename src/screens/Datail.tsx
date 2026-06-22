ce2de475c5c1162b3ae7660b243cf15c0e87fa9d
Guilherme Gaspar Barbosa de Souza <143048590+GuilhermeGasparBarbosa@users.noreply.github.com>
2026-06-22 20:51:41 -0300

    feacture: Melhoria da navegacao e criacao das variáveis de style

--- src/screens/Datail.tsx
+++ src/screens/Datail.tsx
@@ -1,10 +1,11 @@
 import { Text } from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { TrouteProps } from "../Routes";
 
 
 
 export const DetailPage = () => {
-    const { params} = useRoute<any>()
+    const { params } = useRoute<TrouteProps<'detail'>>()
 
     return (<>
         <Text>Detail {params.rate}</Text>
---
+++ src/shared/Components/themes/theme.ts
@@ -0,0 +1,30 @@
+import { Background } from "@react-navigation/elements";
+
+export const theme = {
+    colors: {
+        background: '#F2F2F2',
+        paper: '#FFFFFF',
+        text: '#000000',
+        textPlaceholder: '#A1A1A1',
+        primary: '#926DFF',
+        primaryText: '#ffffff',
+        error: '#ff0000',
+        highlight: '#e0d53b',
+        backgroundHighLight: 'rgba(244, 213, 59, 0.17)',
+    },
+    fonts: {
+        sizes: {
+            tittle: 24,
+            subtittle: 28,
+            body: 14,
+        },
+        family: {
+            regular: '',
+            bold: '',
+            italic: '',
+        },
+    shadows: {
+        default: '0 0 4 0 rgba(0, 0, 0, 0.25)'
+    }
+    },
+}
